946c6069a31ed2c9496c5b5158f3a4b68a1cafaf
Aslam <palakkalaslam@outlook.com>
2015-07-06 13:15:10 +0400

    Init Commit

---
+++ UserIdentityModificationTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UserIdentityModificationTest.Startup))]
+namespace UserIdentityModificationTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
